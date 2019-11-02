fd069819c2977b27a2f26c12c490ef366b4753f1
mostafa ayman ahmed <mostafaayman010030@gmail.com>
2019-11-02 23:04:54 +0200

    first game done

---
+++ guss the number/VC2.swift
@@ -0,0 +1,30 @@
+//
+//  VC2.swift
+//  guss the number
+//
+//  Created by Moustafa on 10/19/19.
+//  Copyright © 2019 Moustafa Ayman Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class VC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ guss the number/vc3.swift
@@ -0,0 +1,30 @@
+//
+//  vc3.swift
+//  guss the number
+//
+//  Created by Moustafa on 10/19/19.
+//  Copyright © 2019 Moustafa Ayman Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class vc3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
